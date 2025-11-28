74043fad329e99209b389db61d75a8cf449e31f3
Paulo Artur <pauloarturvillaca@gmail.com>
2025-11-28 17:14:43 -0300

    histograma2d

--- preparando_dados.py
+++ preparando_dados.py
@@ -1,7 +1,30 @@
 import pandas as pd
 dados_brasil = pd.read_csv("dados.csv", sep=';', encoding='latin1')
-população_campos = dados_brasil[dados_brasil["NO_MUNICIPIO_ESC"].str.lower() == "campos dos goytacazes"][['NU_NOTA_MT', 'NU_NOTA_CN', 'NU_NOTA_REDACAO']]
+população_campos = dados_brasil[dados_brasil["NO_MUNICIPIO_ESC"].str.lower() == "campos dos goytacazes"]
 
-amostra = população_campos.dropna().sort_values(by='NU_NOTA_MT')
+acertos_mt = []
+acertos_cn = []
+acertos_ch = []
+acertos_lc = []
+
+população_campos = população_campos.dropna()
+
+for _, aluno in população_campos.iterrows():
+    pegarAcertos = lambda respostas, gabarito: sum(1 for i in range(len(aluno[respostas])) if aluno[respostas][i] == aluno[gabarito][i])
+
+    acertos_mt += [pegarAcertos("TX_RESPOSTAS_MT", "TX_GABARITO_MT")]
+    acertos_cn += [pegarAcertos("TX_RESPOSTAS_CN", "TX_GABARITO_CN")]
+    acertos_ch += [pegarAcertos("TX_RESPOSTAS_CH", "TX_GABARITO_CH")]
+    acertos_lc += [pegarAcertos("TX_RESPOSTAS_LC", "TX_GABARITO_LC")]
+
+
+amostra = população_campos[["NU_NOTA_MT", "NU_NOTA_CN", "NU_NOTA_CH", "NU_NOTA_LC"]]
+
+amostra["ACERTOS_MT"] = acertos_mt
+amostra["ACERTOS_CN"] = acertos_cn
+amostra["ACERTOS_CH"] = acertos_ch
+amostra["ACERTOS_LC"] = acertos_lc
+
+amostra = amostra.sort_values(by='NU_NOTA_MT')
 
 amostra.to_csv('notas-campos.csv', index=False)
